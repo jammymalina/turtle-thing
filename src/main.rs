6ca5684e016a26f82588ca28ad06575bc4fb0df4
jammymalina <jakub.jantosik@gmail.com>
2024-10-28 18:39:16 +0100

    Update

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ mod turtle;
 
 #[macroquad::main("turtle")]
 async fn main() {
+    request_new_screen_size(1280.0, 720.0);
+
     let t = turtle::Turtle::init(20.0, 20.0);
     loop {
         clear_background(BLACK);
--- src/turtle.rs
+++ src/turtle.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::ops::{Add, Mul, Neg, Sub};
 
 struct Vec2 {
@@ -12,6 +13,15 @@ impl Vec2 {
 
     pub fn len(self) -> f64 {
         return f64::hypot(self.x, self.y);
+    }
+
+    pub fn rotate(self, degrees: f64) -> Vec2 {
+        let perp = Vec2::init(-self.y, self.x);
+        let radians = degrees * std::f64::consts::PI / 180.0;
+
+        let sin = radians.sin();
+        let cos = radians.cos();
+        Vec2::init(self.x * cos + perp.x * sin, self.y * cos + perp.y * sin)
     }
 }
 
@@ -66,6 +76,98 @@ impl Mul<Vec2> for Vec2 {
         Self {
             x: other.x * self.x,
             y: other.y * self.y,
+        }
+    }
+}
+
+enum Shape {
+    Polygon(Vec<(f64, f64)>),
+}
+
+struct TurtleScreen {
+    shapes: HashMap<&'static str, Shape>,
+}
+
+impl TurtleScreen {
+    pub fn init() -> Self {
+        Self {
+            shapes: HashMap::from([
+                (
+                    "arrow",
+                    Shape::Polygon(vec![(-10.0, 0.0), (10.0, 0.0), (0.0, 10.0)]),
+                ),
+                (
+                    "turtle",
+                    Shape::Polygon(vec![
+                        (0.0, 16.0),
+                        (-2.0, 14.0),
+                        (-1.0, 10.0),
+                        (-4.0, 7.0),
+                        (-7.0, 9.0),
+                        (-9.0, 8.0),
+                        (-6.0, 5.0),
+                        (-7.0, 1.0),
+                        (-5.0, -3.0),
+                        (-8.0, -6.0),
+                        (-6.0, -8.0),
+                        (-4.0, -5.0),
+                        (0.0, -7.0),
+                        (4.0, -5.0),
+                        (6.0, -8.0),
+                        (8.0, -6.0),
+                        (5.0, -3.0),
+                        (7.0, 1.0),
+                        (6.0, 5.0),
+                        (9.0, 8.0),
+                        (7.0, 9.0),
+                        (4.0, 7.0),
+                        (1.0, 10.0),
+                        (2.0, 14.0),
+                    ]),
+                ),
+                (
+                    "circle",
+                    Shape::Polygon(vec![
+                        (10.0, 0.0),
+                        (9.51, 3.09),
+                        (8.09, 5.88),
+                        (5.88, 8.09),
+                        (3.09, 9.51),
+                        (0.0, 10.0),
+                        (-3.09, 9.51),
+                        (-5.88, 8.09),
+                        (-8.09, 5.88),
+                        (-9.51, 3.09),
+                        (-10.0, 0.0),
+                        (-9.51, -3.09),
+                        (-8.09, -5.88),
+                        (-5.88, -8.09),
+                        (-3.09, -9.51),
+                        (-0.00, -10.00),
+                        (3.09, -9.51),
+                        (5.88, -8.09),
+                        (8.09, -5.88),
+                        (9.51, -3.09),
+                    ]),
+                ),
+                (
+                    "square",
+                    Shape::Polygon(vec![
+                        (10.0, -10.0),
+                        (10.0, 10.0),
+                        (-10.0, 10.0),
+                        (-10.0, -10.0),
+                    ]),
+                ),
+                (
+                    "triangle",
+                    Shape::Polygon(vec![(10.0, -5.77), (0.0, 11.55), (-10.0, -5.77)]),
+                ),
+                (
+                    "classic",
+                    Shape::Polygon(vec![(0.0,0.0),(-5.0,-9.0),(0.0,-7.0),(5.0,-9.0)])
+                )
+            ]),
         }
     }
 }
